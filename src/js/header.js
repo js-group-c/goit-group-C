8d8dacb648cb33fb87e0fda70684e229f5e981d0
ipekcan <ipekcan@hotmail.com>
2025-02-04 17:46:00 +0300

    prettier

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,11 @@
-
-
 var modal = document.getElementById('login-modal');
 var btn = document.getElementById('btn-sign-up');
 var span = document.getElementsByClassName('close-modal')[0];
 
-var submitBtn = document.getElementsByClassName("submit-button")[0];
+var submitBtn = document.getElementsByClassName('submit-button')[0];
 
-btn.addEventListener("click",btn.onclick);
-submitBtn.addEventListener("click", signUp);
+btn.addEventListener('click', btn.onclick);
+submitBtn.addEventListener('click', signUp);
 
 btn.onclick = function () {
   modal.style.display = 'flex';
