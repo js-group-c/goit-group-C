a0621c67be4a0392cc821b7633e21aefb91149b0
ipekcan <ipekcan@hotmail.com>
2025-02-09 00:46:03 +0300

    satır düzeltme

--- src/js/header.js
+++ src/js/header.js
@@ -27,7 +27,8 @@ span.onclick = function () {
   modal.style.display = 'none';
 };
 window.onclick = function (event) {
-  if (event.target == modal) modal.style.display = 'none';
+  if (event.target == modal) 
+    modal.style.display = 'none';
 };
 
 function signUp() {
