de260601529da2fe63041b5ad4b4be4d9dbb1d7d
nilaytr <nilaytr89@gmail.com>
2025-02-11 15:03:03 +0000

    Deploying to gh-pages from @ js-group-c/goit-group-C@5f10828f37609d3ed0c91a37074b1e14b692647d 🚀

---
+++ shopping.js
@@ -0,0 +1,22 @@
+import{g as u}from"./assets/support-ukr-DM50oc6P.js";import"./assets/vendor-C2dPOmMF.js";let i=JSON.parse(localStorage.getItem("shoppingList"))||[];const c=document.getElementById("book-container"),d=document.querySelector(".empty-list");let l=4,e=1;async function o(){if(i.length===0){d.style.display="block",c.innerHTML="",g();return}else d.style.display="none";c.innerHTML="";const t=i.filter(n=>n!=null&&n!=="");if(t.length===0){d.style.display="block",c.innerHTML="",g();return}try{const n=await u(t);b(n),g()}catch(n){console.error("Error",n)}}function b(t){c.innerHTML="";const n=(e-1)*l,a=n+l;t.slice(n,a).forEach((s,p)=>{const r=document.createElement("div");r.classList.add("book-card"),r.setAttribute("data-index",p+1),r.innerHTML=`
+      <img src="${s.book_image}" alt="${s.title}" class="book-cover">
+      <div class="book-info">
+        <button class="delete-btn" data-id="${s._id}">&#128465;</button>
+        <h3>${s.title}</h3>
+        <p class="category">${s.list_name}</p>
+        <p class="description">${s.description}</p>
+        <p class="author">${s.author}</p>
+        <a href="${s.buy_links[0].url}" target="_blank" class="buy-button"><img src="../img/png/Amazon_logo.png" alt="icon" class="amazon-icon"></a>
+      </div>
+    `,c.appendChild(r)})}function g(){const t=Math.ceil(i.length/l),n=document.getElementById("pagination-container");if(n.innerHTML="",!(t<=1)){n.innerHTML+=`
+    <button class="pagination-btn first-page" ${e===1?"disabled":""}>⏮</button>
+  `,n.innerHTML+=`
+    <button class="pagination-btn prev-page" ${e===1?"disabled":""}>◀</button>
+  `;for(let a=Math.max(1,e-2);a<=Math.min(t,e+2);a++)n.innerHTML+=`
+      <button class="pagination-btn page-number ${a===e?"active":""}" data-page="${a}">${a}</button>
+    `;n.innerHTML+=`
+    <button class="pagination-btn next-page" ${e===t?"disabled":""}>▶</button>
+  `,n.innerHTML+=`
+    <button class="pagination-btn last-page" ${e===t?"disabled":""}>⏭</button>
+  `}}document.addEventListener("click",function(t){t.target.classList.contains("page-number")?(e=parseInt(t.target.dataset.page),o()):t.target.classList.contains("first-page")?(e=1,o()):t.target.classList.contains("prev-page")?e>1&&(e--,o()):t.target.classList.contains("next-page")?e<Math.ceil(i.length/l)&&(e++,o()):t.target.classList.contains("last-page")&&(e=Math.ceil(i.length/l),o())});document.addEventListener("DOMContentLoaded",o);document.getElementById("book-container").addEventListener("click",function(t){if(t.target.classList.contains("delete-btn")){const n=t.target.dataset.id;i=i.filter(a=>a!==n),f(i),o()}});function f(t){localStorage.setItem("shoppingList",JSON.stringify(t))}
+//# sourceMappingURL=shopping.js.map
