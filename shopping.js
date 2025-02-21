69152001d1e84fe087a4eff34e8eabcd4d947923
nilaytr <nilaytr89@gmail.com>
2025-02-21 14:03:12 +0000

    Deploying to gh-pages from @ js-group-c/goit-group-C@bb13de9ace7448add2cb58cb11ea8187e477c81e 🚀

--- shopping.js
+++ shopping.js
@@ -1,4 +1,4 @@
-import{g as u}from"./assets/support-ukr-J0-nDD3z.js";import"./assets/vendor-C2dPOmMF.js";let i=JSON.parse(localStorage.getItem("shoppingList"))||[];const c=document.getElementById("book-container"),d=document.querySelector(".empty-list"),b={Amazon:"img/amazon.svg"};let r=4,e=1;async function o(){if(i.length===0){d.style.display="block",c.innerHTML="",g();return}else d.style.display="none";c.innerHTML="";const t=i.filter(n=>n!=null&&n!=="");if(t.length===0){d.style.display="block",c.innerHTML="",g();return}try{const n=await u(t);m(n),g()}catch(n){console.error("Error",n)}}function m(t){c.innerHTML="";const n=(e-1)*r,a=n+r;t.slice(n,a).forEach((s,p)=>{const l=document.createElement("div");l.classList.add("book-card"),l.setAttribute("data-index",p+1),l.innerHTML=`
+import{g as b}from"./assets/support-ukr-J0-nDD3z.js";import"./assets/vendor-C2dPOmMF.js";let i=JSON.parse(localStorage.getItem("shoppingList"))||[];const c=document.getElementById("book-container"),d=document.querySelector(".empty-list"),p={Amazon1:"img/amazon.svg",Amazon2:"img/amazon-dark.svg"};let r=4,a=1;async function o(){if(i.length===0){d.style.display="block",c.innerHTML="",g();return}else d.style.display="none";c.innerHTML="";const t=i.filter(n=>n!=null&&n!=="");if(t.length===0){d.style.display="block",c.innerHTML="",g();return}try{const n=await b(t);m(n),g()}catch(n){console.error("Error",n)}}function m(t){c.innerHTML="";const n=(a-1)*r,e=n+r;t.slice(n,e).forEach((s,u)=>{const l=document.createElement("div");l.classList.add("book-card"),l.setAttribute("data-index",u+1),l.innerHTML=`
       <img src="${s.book_image}" alt="${s.title}" class="book-cover">
       <div class="book-info">
         <button class="delete-btn" data-id="${s._id}">&#128465;</button>
@@ -6,17 +6,18 @@ import{g as u}from"./assets/support-ukr-J0-nDD3z.js";import"./assets/vendor-C2dP
         <p class="category">${s.list_name}</p>
         <p class="description">${s.description}</p>
         <p class="author">${s.author}</p>    
-        <a href="${s.buy_links[0].url}" target="_blank" class="buy-button"><img src="${b.Amazon}"  alt="icon" class="amazon-icon"></a>
+        <a href="${s.buy_links[0].url}" target="_blank" class="buy-button"><img src="${p.Amazon1}"  alt="icon" class="amazon-icon"></a>
+        <a href="${s.buy_links[0].url}" target="_blank" class="buy-button"><img src="${p.Amazon2}"  alt="icon" class="amazon-icon-dark"></a>
       </div>
     `,c.appendChild(l)})}function g(){const t=Math.ceil(i.length/r),n=document.getElementById("pagination-container");if(n.innerHTML="",!(t<=1)){n.innerHTML+=`
-    <button class="pagination-btn first-page" ${e===1?"disabled":""}>⏮</button>
+    <button class="pagination-btn first-page" ${a===1?"disabled":""}>⏮</button>
   `,n.innerHTML+=`
-    <button class="pagination-btn prev-page" ${e===1?"disabled":""}>◀</button>
-  `;for(let a=Math.max(1,e-2);a<=Math.min(t,e+2);a++)n.innerHTML+=`
-      <button class="pagination-btn page-number ${a===e?"active":""}" data-page="${a}">${a}</button>
+    <button class="pagination-btn prev-page" ${a===1?"disabled":""}>◀</button>
+  `;for(let e=Math.max(1,a-2);e<=Math.min(t,a+2);e++)n.innerHTML+=`
+      <button class="pagination-btn page-number ${e===a?"active":""}" data-page="${e}">${e}</button>
     `;n.innerHTML+=`
-    <button class="pagination-btn next-page" ${e===t?"disabled":""}>▶</button>
+    <button class="pagination-btn next-page" ${a===t?"disabled":""}>▶</button>
   `,n.innerHTML+=`
-    <button class="pagination-btn last-page" ${e===t?"disabled":""}>⏭</button>
-  `}}document.addEventListener("click",function(t){t.target.classList.contains("page-number")?(e=parseInt(t.target.dataset.page),o()):t.target.classList.contains("first-page")?(e=1,o()):t.target.classList.contains("prev-page")?e>1&&(e--,o()):t.target.classList.contains("next-page")?e<Math.ceil(i.length/r)&&(e++,o()):t.target.classList.contains("last-page")&&(e=Math.ceil(i.length/r),o())});document.addEventListener("DOMContentLoaded",o);document.getElementById("book-container").addEventListener("click",function(t){if(t.target.classList.contains("delete-btn")){const n=t.target.dataset.id;i=i.filter(a=>a!==n),f(i),o()}});function f(t){localStorage.setItem("shoppingList",JSON.stringify(t))}
+    <button class="pagination-btn last-page" ${a===t?"disabled":""}>⏭</button>
+  `}}document.addEventListener("click",function(t){t.target.classList.contains("page-number")?(a=parseInt(t.target.dataset.page),o()):t.target.classList.contains("first-page")?(a=1,o()):t.target.classList.contains("prev-page")?a>1&&(a--,o()):t.target.classList.contains("next-page")?a<Math.ceil(i.length/r)&&(a++,o()):t.target.classList.contains("last-page")&&(a=Math.ceil(i.length/r),o())});document.addEventListener("DOMContentLoaded",o);document.getElementById("book-container").addEventListener("click",function(t){if(t.target.classList.contains("delete-btn")){const n=t.target.dataset.id;i=i.filter(e=>e!==n),f(i),o()}});function f(t){localStorage.setItem("shoppingList",JSON.stringify(t))}
 //# sourceMappingURL=shopping.js.map
